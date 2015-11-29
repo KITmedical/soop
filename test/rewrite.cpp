783390bf4310f36efc7b1e29552a2853d39b8580
Florian Weber <oss@florianjw.de>
2015-11-29 16:47:37 +0100

    use aliases in testcase to demonstrate namespace-free code

--- test/rewrite.cpp
+++ test/rewrite.cpp
@@ -33,15 +33,13 @@ TEST_CASE("simple axioms") {
 }
 
 TEST_CASE("complex axioms") {
-	namespace sp = soop::preds;
-	soop::ontology o{p::testpred};
-	o.add_axiom(sp::forall({"x", "y", "z"},
-	                       sp::implies(sp::and_(p::testpred("x", "y"), p::testpred("y", "z")),
-	                                   p::testpred("x", "z"))));
-	o.add_axiom(sp::forall({"x"}, p::testpred("x", "x")));
-	o.add_axiom(sp::forall({"x", "y"},
-	                       sp::implies(sp::and_(p::testpred("x", "y"), p::testpred("y", "x")),
-	                                   sp::equal("x", "y"))));
+	using namespace soop::preds;
+	const auto tp = preds::testpred;
+	soop::ontology o{tp};
+	o.add_axiom(
+	        forall({"x", "y", "z"}, implies(and_(tp("x", "y"), tp("y", "z")), tp("x", "z"))));
+	o.add_axiom(forall({"x"}, tp("x", "x")));
+	o.add_axiom(forall({"x", "y"}, implies(and_(tp("x", "y"), tp("y", "x")), equal("x", "y"))));
 	o.add_type<soop::e<int>>();
 	soop::e<int> e1{nullptr, 1};
 	soop::e<int> e2{nullptr, 2};
@@ -53,14 +51,14 @@ TEST_CASE("complex axioms") {
 	o.add_entity(e3);
 	o.add_entity(e4);
 
-	o.add_axiom(p::testpred(e1, e2));
-	o.add_axiom(p::testpred(e2, e3));
-	o.add_axiom(p::testpred(e3, e2));
-	o.add_axiom(p::testpred(e3, e4));
+	o.add_axiom(tp(e1, e2));
+	o.add_axiom(tp(e2, e3));
+	o.add_axiom(tp(e3, e2));
+	o.add_axiom(tp(e3, e4));
 
-	CHECK(o.request(sp::equal(e2, e3)));
-	CHECK(o.request(p::testpred(e1, e4)));
-	CHECK(o.request(sp::exists({"x"}, sp::and_(p::testpred(e1, "x"), p::testpred("x", e4)))));
+	CHECK(o.request(equal(e2, e3)));
+	CHECK(o.request(tp(e1, e4)));
+	CHECK(o.request(exists({"x"}, and_(tp(e1, "x"), tp("x", e4)))));
 	// Not yet working because we also need to narrow down y to e<int>'s:
-	//CHECK(o.request(sp::exists({"x"}, sp::forall({"y"}, p::testpred("x", "y")))));
+	// CHECK(o.request(sp::exists({"x"}, sp::forall({"y"}, tp("x", "y")))));
 }
