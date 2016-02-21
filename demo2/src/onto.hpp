fa8ad7bd6c81731cdcf1f991fcfb098d3f277f47
Florian Weber <oss@florianjw.de>
2016-02-21 01:06:20 +0100

    make predicates in demo2 typechecked

--- demo2/src/onto.hpp
+++ demo2/src/onto.hpp
@@ -6,9 +6,12 @@
 #include <cstdint>
 
 #include "../../include/soop/onto.hpp"
+
+#include "data.hpp"
+
 soop::ontology make_ontology();
 
-SOOP_MAKE_PREDICATE(is_speaker_of, 2);
-SOOP_MAKE_PREDICATE(talk_assignment, 4);
+SOOP_MAKE_TYPECHECKED_PREDICATE(is_speaker_of, 2, speaker, talk);
+SOOP_MAKE_TYPECHECKED_PREDICATE(talk_assignment, 4, talk, speaker, room, slot);
 
 #endif
